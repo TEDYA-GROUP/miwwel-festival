4088a94c768df03b89ff073b900abf745dd55617
Gilles Yao <gillesyao12@gmail.com>
2022-02-21 13:16:15 +0000

    update: make footer and header responsive

---
+++ dist/js/app.js
@@ -0,0 +1,22 @@
+class App {
+
+  constructor() {
+    this.menuToggler = document.querySelector('.menu-toggler')
+  }
+
+  init() {
+    if (this.menuToggler) {
+      this.menuToggler.querySelector('.hamburger').addEventListener('click', () => {
+        this.menuToggler.classList.add('show');
+      })
+      this.menuToggler.querySelector('.close').addEventListener('click', () => {
+        console.log(this.menuToggler.classList)
+        this.menuToggler.classList.remove('show');
+      })
+    }
+    
+  }
+}
+
+const app = new App();
+app.init();
