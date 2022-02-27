0dfba4a087184d1c9bc225801ecb117f01452bb4
Gilles Yao <gillesyao12@gmail.com>
2022-02-27 17:12:49 +0000

    update: 27-02-22

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,6 +2,9 @@ class App {
 
   constructor() {
     this.menuToggler = document.querySelector('.menu-toggler')
+    this.menus = document.querySelectorAll('.header .navbar-nav a')
+    this.scrollTopButton = document.querySelector('.scroll-top-button')
+    this.settingButtons = document.querySelectorAll('.theme-setting button')
   }
 
   init() {
@@ -14,6 +17,62 @@ class App {
       })
     }
     
+    if (this.menus.length > 0) {
+      this.menus.forEach(item => {
+        item.addEventListener('click', (event) => {
+          event.preventDefault();
+          let targetBloc = document.querySelector(item.getAttribute('href'))
+          console.log(targetBloc)
+          if (targetBloc) {
+            this.scrollTo(targetBloc)
+          }
+          
+        })
+      })
+    }
+
+    if (this.settingButtons.length > 0) {
+      this.settingButtons.forEach(item => {
+        if (item.classList.contains('close')) {
+          item.addEventListener('click', () => {
+            document.querySelector('.theme-setting').style.display = 'none'
+          })
+        }
+        if (item.classList.contains('with-counter')) {
+          item.addEventListener('click', () => {
+            document.querySelector('.counter-wrapper').classList.add('active-counter')
+          })
+        }
+        if (item.classList.contains('without-counter')) {
+          item.addEventListener('click', () => {
+            document.querySelector('.counter-wrapper').classList.remove('active-counter')
+          })
+        }
+      })
+    }
+
+    if (this.scrollTopButton) {
+      window.addEventListener('scroll', (event)=> {
+        if (window.scrollY <= 400) {
+          this.scrollTopButton.style.display = 'none'
+        } else {
+          this.scrollTopButton.style.display = 'flex'
+        }
+      })
+
+      this.scrollTopButton.addEventListener('click', event => {
+        event.preventDefault();
+        this.scrollTo(document.body)
+      })
+    }
+  }
+
+  scrollTo(to) {
+    window.scroll({
+      behavior: 'smooth',
+      left: 0,
+      top: to.offsetTop,
+    })
   }
 }
 
